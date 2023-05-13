e3f945858ddb53acca9b0aeaa7db4fb9694e4959
Eugene Golnev <eugenegolnev1993@gmail.com>
2023-05-14 00:43:02 +0200

    3_2 refactoring

--- section_3/3_2/3_2_Задание оформляем тесты в стиле unittest.py
+++ section_3/3_2/3_2_Задание оформляем тесты в стиле unittest.py
@@ -4,37 +4,42 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
+def inputs(browser):
+    input_1 = browser.find_element(By.CSS_SELECTOR, '.first_block .first')
+    input_1.send_keys('test.txt')
+
+    input_2 = browser.find_element(By.CSS_SELECTOR, '.first_block .second')
+    input_2.send_keys('test.txt')
+
+    input_3 = browser.find_element(By.CSS_SELECTOR, '.first_block .third')
+    input_3.send_keys('test.txt')
+
+    button = browser.find_element(By.CSS_SELECTOR, 'button.btn')
+    button.click()
+
+    welcome_text = browser.find_element(By.TAG_NAME, 'h1').text
+
+    return welcome_text
+
+
 class TestLink(unittest.TestCase):
-    browser = webdriver.Chrome()
-
-    def inputs(self):
-        input_1 = self.browser.find_element(By.CSS_SELECTOR, '.first_block .first')
-        input_1.send_keys('test.txt')
-
-        input_2 = self.browser.find_element(By.CSS_SELECTOR, '.first_block .second')
-        input_2.send_keys('test.txt')
-
-        input_3 = self.browser.find_element(By.CSS_SELECTOR, '.first_block .third')
-        input_3.send_keys('test.txt')
-
-        button = self.browser.find_element(By.CSS_SELECTOR, 'button.btn')
-        button.click()
-
-        welcome_text = self.browser.find_element(By.TAG_NAME, 'h1').text
-
-        self.assertEqual(welcome_text, 'Congratulations! You have successfully registered!', f'expected {welcome_text}')
-
     def test_link_1(self):
         link = 'http://suninjuly.github.io/registration1.html'
-        self.browser.get(link)
+        browser = webdriver.Chrome()
+        browser.get(link)
 
-        self.inputs()
+        welcome_text = inputs(browser)
+        self.assertEqual(welcome_text, 'Congratulations! You have successfully registered!', f'expected {welcome_text}')
+        browser.quit()
 
     def test_link_2(self):
         link = 'http://suninjuly.github.io/registration2.html'
-        self.browser.get(link)
+        browser = webdriver.Chrome()
+        browser.get(link)
 
-        self.inputs()
+        welcome_text = inputs(browser)
+        self.assertEqual(welcome_text, 'Congratulations! You have successfully registered!', f'expected {welcome_text}')
+        browser.quit()
 
 
 if __name__ == '__main__':
