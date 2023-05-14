9052fed19bc9e222aadae74dd45c41c3e78a87cb
Eugene Golnev <eugenegolnev1993@gmail.com>
2023-05-14 20:49:32 +0200

    finish 3_5 (Марекровка, Пропуск, XFail)

---
+++ section_3/3_5/test_fixture81.py
@@ -0,0 +1,32 @@
+import pytest
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+link = 'http://selenium1py.pythonanywhere.com/'
+
+
+@pytest.fixture(scope='function')
+def browser():
+    print('\nstart browser for test..')
+    browser = webdriver.Chrome()
+    yield browser
+    print('\nquit browser..')
+    browser.quit()
+
+
+class TestMainPage:
+
+    @pytest.mark.smoke
+    def test_guest_should_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, '#lodin_link')
+
+    @pytest.mark.smoke
+    @pytest.mark.win11
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, '.basket-mini .btn-group > a')
+
+#  Запуск: pytest -s -v -m "smoke and win11" test_fixture81.py
