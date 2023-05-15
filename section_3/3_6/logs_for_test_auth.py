ddb24056233210dca66a7469b1adf157a5eeabdd
Eugene Golnev <eugenegolnev1993@gmail.com>
2023-05-15 20:19:16 +0200

    add 3_6 (конфигурация, параметризация тестов)

---
+++ section_3/3_6/logs_for_test_auth.py
@@ -0,0 +1,2 @@
+login = 'eugenegolnev1993@gmail.com'
+password = '255525GF'
---
+++ section_3/3_6/test_authorization.py
@@ -0,0 +1,30 @@
+import pytest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import logs_for_test_auth
+
+
+link = 'https://stepik.org/lesson/236895/step/1'
+
+
+@pytest.fixture
+def log_in_stepic(browser):
+    browser.implicitly_wait(5)
+    browser.get(link)
+    login_button = WebDriverWait(browser, 5).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, '#ember33'))
+    )
+    login_button.click()
+
+    browser.find_element(By.ID, 'id_login_email').send_keys(logs_for_test_auth.login)
+    browser.find_element(By.ID, 'id_login_password').send_keys(logs_for_test_auth.password)
+
+    browser.find_element(By.CSS_SELECTOR, 'button.sign-form__btn').click()
+
+    return browser
+
+
+def test_parametrisation_test(log_in_stepic):
+    log_in_stepic.find_element(By.CSS_SELECTOR, '.navbar__profile-img')
